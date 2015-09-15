911327410d900aa5e55834be688437d1f6e008de
lihe11 <lihe1163@163.com>
2015-09-15 12:39:34 +0800

    Revert "菜单管理"
    This reverts commit c11e0c9befe27890fd5cf1fab85867b34eb11032.

--- src/main/java/test/test1/TreeBeanRec.java
+++
@@ -1,8 +0,0 @@
-package test.test1;
-
-/**
- * Created by lihe
- * on 2015/8/5.17:38
- */
-public class TreeBeanRec {
-}
--- src/main/java/test/test1/TreeService.java
+++
@@ -1,8 +0,0 @@
-package test.test1;
-
-/**
- * Created by lihe
- * on 2015/8/5.17:40
- */
-public class TreeService {
-}
